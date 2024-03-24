a24c7a9fd8ca5c56dae2296d443a2615a16ce3c0
Torkus <48141663+ogri-la@users.noreply.github.com>
2024-03-24 11:28:12 +1030

    settled on httpreaderat and buf-readerat

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module readzip
 
 go 1.21.6
+
+require (
+	github.com/avvmoto/buf-readerat v0.0.0-20171115124131-a17c8cb89270
+	github.com/snabb/httpreaderat v1.0.1
+)
+
+require github.com/pkg/errors v0.8.1 // indirect
--- main.go
+++ main.go
@@ -2,28 +2,64 @@ package main
 
 import (
 	"archive/zip"
+	"context"
 	"fmt"
-	"os"
+	"io"
+	"net/http"
+	"net/http/httptrace"
+
+	bufra "github.com/avvmoto/buf-readerat"
+	"github.com/snabb/httpreaderat"
 )
 
 func main() {
 
-	rdr, err := os.Open("bigfile.zip")
+	// client trace to log whether the request's underlying tcp connection was re-used
+	clientTrace := &httptrace.ClientTrace{
+		GotConn: func(info httptrace.GotConnInfo) {
+			fmt.Printf("reused %s conn? %v\n", info.Conn.RemoteAddr(), info.Reused)
+		},
+	}
+	traceCtx := httptrace.WithClientTrace(context.Background(), clientTrace)
+
+	// ---
+
+	url := "https://github.com/ogri-la/ziptest/releases/download/0.0.1/the-undermine-journal--5-6-20220531.zip"
+
+	client := http.Client{}
+	req, err := http.NewRequestWithContext(traceCtx, http.MethodGet, url, nil)
 	if err != nil {
-		panic("failed to open file: " + err.Error())
+		panic("error creating request: " + err.Error())
 	}
 
-	finfo, err := rdr.Stat()
+	// ---
+
+	http_readerat, err := httpreaderat.New(&client, req, nil)
 	if err != nil {
-		panic("failed to read file info: " + err.Error())
+		panic("error creating HTTPReaderAt: " + err.Error())
 	}
 
-	zrdr, err := zip.NewReader(rdr, finfo.Size())
+	buffered_http_readerat := bufra.NewBufReaderAt(http_readerat, 1024*1024)
+	zip_reader, err := zip.NewReader(buffered_http_readerat, http_readerat.Size())
 	if err != nil {
-		panic("failed to open a zip reader: " + err.Error())
+		panic("error creating zip.Reader: " + err.Error())
 	}
 
-	for _, f := range zrdr.File {
-		fmt.Println(f.FileInfo())
+	for _, f := range zip_reader.File {
+		fmt.Println(f.Name)
+		if f.Name == "TheUndermineJournal/TheUndermineJournal.toc" {
+			fmt.Println("---")
+			fh, err := f.Open()
+			if err != nil {
+				panic("error opening zipfile entry: " + err.Error())
+			}
+
+			bl, err := io.ReadAll(fh)
+			if err != nil {
+				panic("error reading bytes from zipfile entry: " + err.Error())
+			}
+			fmt.Println(string(bl))
+			fmt.Println("---")
+		}
 	}
 }
